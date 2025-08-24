86d91514ad155a42b19e444ed9fb0c9b2563b1af
fxosie-8974 <fxosie@gmail.com>
2025-08-24 10:43:13 +0900

    add adsense verification head

---
+++ src/app/head.tsx
@@ -0,0 +1,15 @@
+export default function Head() {
+  return (
+    <>
+      <title>Ronin Price Widgets</title>
+      <meta name="description" content="COIN / RICE / RONKE monitor" />
+      
+      {/* ✅ AdSense 確認用コード */}
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7486115105644729"
+        crossOrigin="anonymous"
+      ></script>
+    </>
+  );
+}
